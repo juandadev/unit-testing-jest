766538560992210f4309dd51e4ff2db2b08b29bf
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2021-05-08 13:28:30 -0600

    Testing for other variable types and callbacks

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,35 @@
 const text = "Hello World";
+const fruits = ["🍎", "🍈", "🍌"];
+const num = 10;
+const bool = true;
 
+// Test fro Strings
 test("Must contain a text", () => {
   expect(text).toMatch(/World/);
+});
+
+// Test for Arrays
+test("Are there a banana?", () => {
+  expect(fruits).toContain("🍌");
+});
+
+// Test for Numbers
+test("Greater than", () => {
+  expect(num).toBeGreaterThan(9);
+});
+
+// Test for Booleans
+test("True", () => {
+  expect(bool).toBeTruthy();
+});
+
+// Test for Callbacks
+const reverseString = (str, callback) => {
+  callback(str.split("").reverse().join(""));
+};
+
+test("Testing a callback", () => {
+  reverseString(text, (str) => {
+    expect(str).toBe("dlroW olleH");
+  });
 });
